4b9431f2c170e68d943f52d70001e7d1cd070d71
hilda <=>
2024-06-05 21:55:15 -0300

    protótipo de página inicial Vue gerada

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+  <h1>Bem-vindo à Rede Social de Sobrevivência Zumbi (ZSSN)</h1>
+  <p>Aqui você pode compartilhar recursos e informações com outros sobreviventes.</p>
+  <router-link to="/survivors" class="btn">Ver Sobreviventes</router-link>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .home {
+    text-align: center;
+    margin-top: 50px;
+  }
+  .btn {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    margin-top: 20px;
+  }
+  .btn:hover {
+    background-color: #0056b3;
+  }
 </style>
